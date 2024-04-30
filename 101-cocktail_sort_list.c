55fb192c7a16397114040791bc1ff8a1b6513b65
soufianetangi <soufianetangi2@gmail.com>
2024-04-30 07:21:41 -0700

    Sorting algorithm

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -7,7 +7,7 @@ void cocktail_sort_list(listint_t **lists);
 /**
  * swap_node_ahead - Swap a node in a listint_t doubly-linked list
  *                   list of integers with the node ahead of it.
- * @list: A pointer to the head of a doubly-linked list of integers.
+ * @lists: A pointer to the head of a doubly-linked list of integers.
  * @tail: A pointer to the tail of the doubly-linked list.
  * @shaker: A pointer to the current swapping node of the cocktail shaker algo.
  */
@@ -33,7 +33,7 @@ void swap_node_ahead(listint_t **lists, listint_t **tail, listint_t **shaker)
 /**
  * swap_node_behind - Swap a node in a listint_t doubly-linked
  *                    list of integers with the node behind it.
- * @list: A pointer to the head of a doubly-linked list of integers.
+ * @lists: A pointer to the head of a doubly-linked list of integers.
  * @tail: A pointer to the tail of the doubly-linked list.
  * @shaker: A pointer to the current swapping node of the cocktail shaker algo.
  */
@@ -59,7 +59,7 @@ void swap_node_behind(listint_t **lists, listint_t **tail, listint_t **shaker)
 /**
  * cocktail_sort_list - Sort a listint_t doubly-linked list of integers in
  *                      ascending order using the cocktail shaker algorithm.
- * @list: A pointer to the head of a listint_t doubly-linked list.
+ * @lists: A pointer to the head of a listint_t doubly-linked list.
  */
 void cocktail_sort_list(listint_t **lists)
 {
